1eae816b263eda286785937eb36ab636957ba767
Jona4325 <javierleon2418@epn.edu.ec>
2025-08-13 10:45:24 -0500

    Profile

--- iap/iap/settings.py
+++ iap/iap/settings.py
@@ -47,18 +47,18 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 
     # Local Apps
-    'iap.academia',
-    'iap.alabanza',
-    'iap.anfitriones',
-    'iap.cunakids',
-    'iap.discipulado',
-    'iap.encuentros',
-    'iap.jap',
-    'iap.multimedia',
-    'iap.pusukids',
-    'iap.pequediks.apps.PequediksConfig',
-    'iap.jpro',
-    'iap.account',
+    'academia',
+    'alabanza',
+    'anfitriones',
+    'cunakids',
+    'discipulado',
+    'encuentros',
+    'jap',
+    'multimedia',
+    'pusukids',
+    'pequediks.apps.PequediksConfig',
+    'jpro',
+    'account',
 ]
 
 # settings.py
