43bffbfe4e56bfcdd025bd91d0f6a4a90485839f
Jona4325 <javierleon2418@epn.edu.ec>
2025-08-15 18:17:17 -0500

    base

--- iap/settings.py
+++ iap/settings.py
@@ -78,8 +78,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'iap.wsgi.application'
 
+# Database configuration to use PostgreSQL with Heroku
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')  # Heroku provides this environment variable
+    )
 }
 
 # Password validation
